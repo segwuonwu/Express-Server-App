696e1412eb7766913b38e040892f06d4f479245b
segwuonwu <segwuonwu@gmail.com>
2020-06-29 14:00:31 -0700

    Add some modules and ejs template

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
+
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (req, res) => {
-    res.send('Hello Express :)')
+    res.render('pages/index', { pageTitle: 'Welcome' });
+});
+
+app.get('/speakers', (req, res) => {
+    res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
