b84ac6dd664626315e71b1929e4c2e21000bfd54
segwuonwu <segwuonwu@gmail.com>
2020-07-03 12:07:17 -0700

    created files to store speakers services. And files for feedback

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
+const speakerRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
 const router = express.Router();
 
 module.exports = () => {
     router.get('/', (req, res) => {
         res.render('pages/index', { pageTitle: 'Welcome' });
     });
+
+    router.use('/speakers', speakerRoute());
+    router.use('/feedback', feedbackRoute());
+
     return router;
 }
 
-// app.get('/speakers', (req, res) => {
-//     res.sendFile(path.join(__dirname, './static/speakers.html'));
-// });
+
---
+++ routes/speakers.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = () => {
+    router.get('/', (req, res) => {
+        return res.send('Speakers list');
+    });
+
+    router.get('/:shortname', (req, res) => {
+        return res.send(`Detail page of ${req.params.shortname}`);
+    });
+
+    return router;
+}
